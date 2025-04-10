55688b7be0ed32e71acb804fc55d0ca05c7c936e
BigguyFM98000 <fm.raphalalani29@gmail.com>
2025-04-10 09:51:27 +0200

    finished the variables.js file

---
+++ variables.js
@@ -0,0 +1,25 @@
+// Exercise 1: Variable Declaration
+let name = "Fhatuwani";
+let age = 35;
+let isStudent = false;
+console.log(name, age, isStudent);
+
+// Exercise 2: Variable
+let favoriteColor = "green";
+console.log(favoriteColor);
+
+favoriteColor = "skyblue";
+console.log(favoriteColor);
+
+// Exercise 3: Arithmetic
+let num1 = 12;
+let num2 = 5;
+let sum = num1 + num2;
+let product = num1 * num2;
+let difference = num1 - num2;
+let quotient = num1 / num2;
+
+console.log(`The sum is ${sum}`);
+console.log(`The difference is ${difference}`);
+console.log(`The product is ${product}`);
+console.log(`The quotient is ${quotient}`);
